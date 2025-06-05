9f2ba8da2be7491f8b6637f132e6826df8beca5d
tiam <2243566770@qq.com>
2025-06-05 15:56:12 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -996,7 +996,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://40.233.6.228:21114".to_owned()
 }
 
 #[inline]
